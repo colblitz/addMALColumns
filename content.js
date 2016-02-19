9bf1f132c6ab22bea3e33a543250dddb97e2f2da
Joseph Lee <colblitz@yext.com>
2016-02-19 17:27:48 -0500

    get ids

--- content.js
+++ content.js
@@ -1,18 +1,30 @@
-var elements = document.getElementsByTagName('*');
+// http://myanimelist.net/animelist/Tigress5
+// http://myanimelist.net/animelist/colblitz
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace("Currently Watching", 'kjsahkeghaiwuehg');
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
+if (!window.jQuery) {
+    console.log("no jquery");
+} else {
+    console.log("yeah has jquery");
 }
+
+var testNewList = function() {
+    if ($(".header-menu").length) {
+        console.log("has header menu");
+        return true;
+    } else {
+        console.log("no header menu");
+        return false;
+    }
+};
+
+var isNewList = testNewList();
+
+if (isNewList) {
+    var data = $('table.list-table').data();
+    var ids = data["items"].map(function(o) { return o["anime_id"]; });
+} else {
+    var more = $('[id^=more');
+    var ids = more.map(function(i, el) { return parseInt($(el).attr('id').replace("more", "")); });
+}
+
+console.log(ids);
