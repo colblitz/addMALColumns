74b3cb155c4e14a6955eea16e6adb8ae8d2489fe
Joseph Lee <z.joseph.lee.z@gmail.com>
2016-02-20 00:05:39 -0500

    add columns

--- content.js
+++ content.js
@@ -27,4 +27,32 @@ if (isNewList) {
     var ids = more.map(function(i, el) { return parseInt($(el).attr('id').replace("more", "")); });
 }
 
+if (isNewList) {
+
+} else {
+    $("#list_surround").width(1100);
+    var headers = $('[class^=header_');
+    headers.map(function(i, el) {
+        var headerTable = $(el).next();
+        var headerRow = headerTable.find("tr");
+        var headerCol = headerTable.find("td").last();
+
+        headerRow.append('<td class="table_header" width="70" align="center" nowrap=""><strong>Tags</strong></td>');
+        headerRow.append('<td class="table_header" width="70" align="center" nowrap=""><strong>Tags</strong></td>');
+        headerRow.append('<td class="table_header" width="70" align="center" nowrap=""><strong>Tags</strong></td>');
+    })
+
+    var more = $('[id^=more');
+    more.map(function(i, el) {
+        var rowTable = $(el).prev();
+        var rowRow = rowTable.find("tr");
+        var rowCol = rowTable.find("td").last();
+        
+        var tdType = rowCol.attr("class");
+        rowRow.append('<td class="' + tdType + '" align="center" width="70"><span id=""></span></td>');
+        rowRow.append('<td class="' + tdType + '" align="center" width="70"><span id=""></span></td>');
+        rowRow.append('<td class="' + tdType + '" align="center" width="70"><span id=""></span></td>');
+    })
+}
+
 console.log(ids);
