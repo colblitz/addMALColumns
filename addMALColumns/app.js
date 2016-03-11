164d59ac61aeae3a47d497fe5dc24ad4e1595f6c
colblitz <z.joseph.lee.z@gmail.com>
2016-03-11 17:28:44 -0500

    testing ajax things

--- addMALColumns/app.js
+++ addMALColumns/app.js
@@ -3,6 +3,7 @@ var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
 var logger       = require('morgan');
+var jquery       = require('jquery');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var mongoose     = require('mongoose');
--- addMALColumns/routes/index.js
+++ addMALColumns/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var jquery = require('jquery');
+var request = require('request');
 var Anime = require('../models/anime');
 var Manga = require('../models/manga');
 var router = express.Router();
@@ -47,12 +49,14 @@ router.post('/requestAnime', function(req, res) {
             console.log(a);
             var index = notIn.indexOf(a.id);
             if (index > -1) {
-                notIn.splice(index, 1);
+                // notIn.splice(index, 1);
             }
         });
         console.log("not in: " + notIn);
         notIn.forEach(function(id) {
         //for (id in notIn) {
+            scrapeAnime(id);
+            //console.log(parseAnimeInfo(scrapeAnime(id)));
             var newAnime = new Anime();
             newAnime.id = id;
             newAnime.title = "asdf";
@@ -61,31 +65,50 @@ router.post('/requestAnime', function(req, res) {
             newAnime.rank = 123;
             newAnime.score = 15.1452;
 
-            newAnime.save(function(err) {
-                if (err) {
-                    console.log("Error saving anime: " + err);
-                    sendErrResponse(res, err);
-                    return;
-                }
-                console.log("Anime successfully saved");
-            });
+            // newAnime.save(function(err) {
+            //     if (err) {
+            //         console.log("Error saving anime: " + err);
+            //         sendErrResponse(res, err);
+            //         return;
+            //     }
+            //     console.log("Anime successfully saved");
+            // });
         });
         sendSuccess(res, anime);
     });
 });
 
 var scrapeAnime = function(id) {
-    return $.ajax({
-        url: "http://myanimelist.net/anime/" + id,
-        type: 'GET',
-        dataType: "html",
-        success: function (data) {
-            return data;
-        },
-        error: function () {
-            console.log("error");
-        }
+    console.log("scraping: " + id + "--------------------------------------------------------------------------------");
+    // request module is used to process the yql url and return the results in JSON format
+    var url = "http://myanimelist.net/anime/" + id;
+
+    console.log("url: " + url);
+    request(url, function(err, resp, body) {
+        console.log(body);
+
+        var a = jquery.parseXML(body);
+        // body = JSON.parse(body);
+        // logic used to compare search results with the input from user
+        // if (!body.query.results.RDF.item) {
+        //     craig = "No results found. Try again.";
+        // } else {
+        //     craig = body.query.results.RDF.item[0]['about'];
+        // }
     });
+
+    // return ajax({
+    //     url: "http://myanimelist.net/anime/" + id,
+    //     type: 'GET',
+    //     dataType: "html",
+    //     success: function (data) {
+    //         console.log("got stuff for: " + id);
+    //         return data;
+    //     },
+    //     error: function () {
+    //         console.log("error");
+    //     }
+    // });
 }
 
 var parseAnimeInfo = function(data) {
@@ -93,6 +116,7 @@ var parseAnimeInfo = function(data) {
     var stat_2 = $($(data.responseText).find("span:contains('Studios')")[0]).next().text();
     var stat_3 = $($(data.responseText).find("span:contains('Ranked')")[0]).next().text();
     var stat_4 = $($(data.responseText).find("span:contains('Score')")[0]).next().text();
+    console.log([stat_1, stat_2, stat_3, stat_4]);
     return [stat_1, stat_2, stat_3, stat_4];
 }
 
