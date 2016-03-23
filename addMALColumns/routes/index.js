057daecca9ac6c5d7c9348b0a16392cc51b815bf
colblitz <z.joseph.lee.z@gmail.com>
2016-03-23 18:55:42 -0400

    callbacks are dumb

--- addMALColumns/routes/index.js
+++ addMALColumns/routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var request = require('request');
 var cheerio = require('cheerio');
+var async = require("async");
+
 var Anime = require('../models/anime');
 var Manga = require('../models/manga');
 
@@ -54,92 +56,182 @@ router.post('/requestAnime', function(req, res) {
         var notIn = req.body.ids.slice();
 
         anime.forEach(function(a) {
-            console.log("retrieved anime: ");
-            console.log(a);
+            console.log("retrieved anime: ", a.id);
             var index = notIn.indexOf(a.id);
             if (index > -1) {
-                // notIn.splice(index, 1);
+                notIn.splice(index, 1);
             }
         });
         console.log("not in: " + notIn);
         console.log("start scrape: ", new Date().getTime());
-        notIn.forEach(function(id) {
-        //for (id in notIn) {
-            scrapeAnime(id);
-            //console.log(parseAnimeInfo(scrapeAnime(id)));
-            // var newAnime = new Anime();
-            // newAnime.id = id;
-            // newAnime.title = "asdf";
-            // newAnime.premiered = "alskdjf";
-            // newAnime.studios = "alksjflajeflk";
-            // newAnime.rank = 123;
-            // newAnime.score = 15.1452;
 
-            // newAnime.save(function(err) {
-            //     if (err) {
-            //         console.log("Error saving anime: " + err);
-            //         sendErrResponse(res, err);
-            //         return;
+async.parallel([
+    function(callback){
+        setTimeout(function(){
+            callback(null, 'one');
+        }, 200);
+    },
+    function(callback){
+        setTimeout(function(){
+            callback(null, 'two');
+        }, 100);
+    }
+],
+// optional callback
+function(err, results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+});
+
+
+        async.forEachOf(notIn, function (value, key, callback) {
+            scrapeAnime(value, function(stats) {
+
+                return;
+            });
+            return;
+
+
+            // fs.readFile(__dirname + value, "utf8", function (err, data) {
+            //     if (err) return callback(err);
+            //     try {
+            //         configs[key] = JSON.parse(data);
+            //     } catch (e) {
+            //         return callback(e);
             //     }
-            //     console.log("Anime successfully saved");
+            //     callback();
             // });
+        }, function (err) {
+            if (err) {
+                console.log("alksdjfd");
+                console.log(err);
+                sendErrResponse(res, err);
+                return;
+            }
+            // if (err) console.error(err.message);
+            // // configs is now a map of JSON data
+            // doSomethingWith(configs);
+            console.log("end scrape: ", new Date().getTime());
+            sendSuccess(res, anime);
+            return;
         });
-        console.log("end scrape: ", new Date().getTime());
-        sendSuccess(res, anime);
+
+        console.log("lkjasdf");
+
+
+
+
+
+        // notIn.forEach(function(id) {
+        // //for (id in notIn) {
+        //     var stats = scrapeAnime(id);
+        //     console.log(stats);
+
+        //     var newAnime = new Anime();
+        //     newAnime.id = id;
+        //     newAnime.title = stats.name;
+        //     newAnime.premiered = stats.premiered;
+        //     newAnime.studios = stats.studio;
+        //     newAnime.rank = stats.rank;
+        //     newAnime.score = stats.score;
+
+        //     newAnime.save(function(err) {
+        //         if (err) {
+        //             console.log("Error saving anime:");
+        //             console.log(newAnime);
+        //             console.log(err);
+        //             sendErrResponse(res, err);
+        //             return;
+        //         }
+        //         //console.log("Anime successfully saved");
+        //     });
+        // });
+        // console.log("end scrape: ", new Date().getTime());
+        // sendSuccess(res, anime);
     });
 });
 
 
-var scrapeAnime = function(id) {
-    console.log("scraping: " + id + "--------------------------------------------------------------------------------");
-    // request module is used to process the yql url and return the results in JSON format
-    var url = "http://myanimelist.net/anime/" + id;
+// var fetch = function(file,cb){
+//      request.get(file, function(err,response,body){
+//            if ( err){
+//                  cb(err);
+//            } else {
+//                  cb(null, body); // First param indicates error, null=> no error
+//            }
+//      });
+// }
+async.map(["file1", "file2", "file3"], fetch, function(err, results){
+    if ( err){
+       // either file1, file2 or file3 has raised an error, so you should not use results and handle the error
+    } else {
+       // results[0] -> "file1" body
+       // results[1] -> "file2" body
+       // results[2] -> "file3" body
+    }
+});
 
-    console.log("url: " + url);
-    request(url, function(err, resp, body) {
-        //console.log(body);
-        parseAnimeInfo(body);
 
-        // var a = jquery.parseXML(body);
-        // var a = $.parseXML(body);
-        // body = JSON.parse(body);
-        // logic used to compare search results with the input from user
-        // if (!body.query.results.RDF.item) {
-        //     craig = "No results found. Try again.";
-        // } else {
-        //     craig = body.query.results.RDF.item[0]['about'];
-        // }
+
+var saveAnime = function(stats) {
+    console.log("saving anime with stats: ", stats);
+
+    var newAnime = new Anime();
+    newAnime.id = value;
+    newAnime.title = stats.name;
+    newAnime.premiered = stats.premiered;
+    newAnime.studios = stats.studio;
+    newAnime.rank = Number(stats.rank);
+    newAnime.score = Number(stats.score);
+
+    newAnime.save(function(err) {
+        if (err) {
+            console.log("Error saving anime:");
+            console.log(newAnime);
+            console.log(err);
+            return err;
+        } else {
+            console.log("Anime successfully saved");
+            return null;
+        }
     });
+}
 
-    // $.ajax({
-    //     url: "http://myanimelist.net/anime/" + id,
-    //     type: 'GET',
-    //     dataType: "html",
-    //     success: function (data) {
-    //         console.log("got stuff for: " + id);
-    //         return data;
-    //     },
-    //     error: function () {
-    //         console.log("error");
-    //     }
-    // });
+
+var scrapeAnime = function(id, callback) {
+    var url = "http://myanimelist.net/anime/" + id;
+    console.log("scraping: " + url);
+    request(url, function(err, resp, body) {
+        if (err) {
+            // err
+        } else {
+            var stats = parseAnimeInfo(body);
+
+            callback(body);
+        }
+    });
 }
 
 var parseAnimeInfo = function(data) {
+    console.log("parsing");
     var $ = cheerio.load(data);
-    var stat_1 = $($("span:contains('Premiered')")[0]).next().text();
-    var stat_2 = $($("span:contains('Studios')")[0]).next().text();
-    var stat_3 = $($("span:contains('Ranked')")[0]).next().text();
-    var stat_4 = $($("span:contains('Score')")[0]).next().text();
-    console.log([stat_1, stat_2, stat_3, stat_4]);
-    return [stat_1, stat_2, stat_3, stat_4];
+    var name = $($('span[itemprop="name"]')[0]).text();
+    var stat_1 = $($("span:contains('Premiered:')")[0]).next().text();
+    var stat_2 = $($("span:contains('Studios:')")[0]).next().text();
+    var stat_3 = $($("span:contains('Ranked:')")[0]).parent().contents().filter(function() {
+        return this.nodeType == 3;
+    }).text().replace(/^\s+|\s+$/g, '').replace('#','');
 
-    // var stat_1 = $($(data.responseText).find("span:contains('Premiered')")[0]).next().text();
-    // var stat_2 = $($(data.responseText).find("span:contains('Studios')")[0]).next().text();
-    // var stat_3 = $($(data.responseText).find("span:contains('Ranked')")[0]).next().text();
-    // var stat_4 = $($(data.responseText).find("span:contains('Score')")[0]).next().text();
-    // console.log([stat_1, stat_2, stat_3, stat_4]);
-    // return [stat_1, stat_2, stat_3, stat_4];
+    var stat_4 = $($("span:contains('Score:')")[0]).next().text();
+    console.log("got stuff");
+    console.log([name, stat_1, stat_2, stat_3, stat_4]);
+    var stats = {
+        name: name,
+        premiered: stat_1,
+        studio: stat_2,
+        rank: stat_3,
+        score: stat_4};
+    return stats;
 }
 
 module.exports = router;
